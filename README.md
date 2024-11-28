aa9e2f3d1658ee2331e1338808d8a88723c4a624
Muslim Helalee <muslim123helalee@gmail.com>
2024-11-28 23:14:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# **useRef vs. useState - React from Confusion to Clarity**
+
+In this video, we are going to code a simple Stopwatch app to showcase the differences between managing states that result in re-renders and persist data without causing re-renders.
+
+## Example App
+
+- Simple Stopwatch app to showcase when to re-render the UI based of changes in the state of the app.
+
+## Taking a Look at the Example App
+
+After forking or downloading it from GitHub, follow these steps:
+
+1. Open with your favorite code editor
+
+2. Install the required dependencies by running the following command:
+
+```
+npm install
+```
+
+3. Start the development server by running the following command:
+
+```
+npm run dev
+```
+
+Happy Coding and Learning 😊
---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import { useRef, useState } from "react";
+
+export default function Stopwatch() {
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const intervalRef = useRef<number | null>(null);
+  const startTimeRef = useRef(0);
+
+  const clearIntervalRef = () => {
+    if (intervalRef.current !== null) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null; // Reset to null
+    }
+  };
+
+  const startStopwatch = () => {
+    setIsRunning(true);
+    setIsPaused(false);
+    startTimeRef.current = Date.now() - elapsedTime;
+    intervalRef.current = setInterval(() => {
+      setElapsedTime(Date.now() - startTimeRef.current);
+    });
+  };
+
+  const stopStopwatch = () => {
+    clearIntervalRef();
+    setIsRunning(false);
+    setIsPaused(true);
+  };
+
+  const resetStopwatch = () => {
+    clearIntervalRef();
+    setIsRunning(false);
+    setIsPaused(false);
+    setElapsedTime(0);
+  };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}.${milliseconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <main className=" flex flex-col gap-12 px-16 py-8 rounded-xl bg-gray-800 shadow-2xl">
+      <h1 className="text-3xl font-bold text-center text-white ">Stopwatch</h1>
+      <p
+        className="text-6xl font-bold text-center text-amber-400"
+        aria-live="polite"
+      >
+        {formatTime(elapsedTime)}
+      </p>
+      <div className="grid grid-cols-[repeat(3,110px)] gap-x-8">
+        <button
+          onClick={startStopwatch}
+          disabled={isRunning}
+          className="bg-[#36BA98] hover:bg-teal-600 text-white font-bold py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          {isPaused ? "Resume" : "Start"}
+        </button>
+
+        <button
+          onClick={stopStopwatch}
+          disabled={!isRunning}
+          className="bg-[#ef9144] hover:bg-amber-600 text-white font-bold py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105  disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          Stop
+        </button>
+
+        <button
+          onClick={resetStopwatch}
+          className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          Reset
+        </button>
+      </div>
+    </main>
+  );
+}
